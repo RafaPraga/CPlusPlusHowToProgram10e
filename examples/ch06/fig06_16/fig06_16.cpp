693bbca722ea19ec23196bb5ada09008449856b0
RafaPraga <rafael.olarte@hotmail.com>
2020-04-17 17:55:34 -0500

    Keyword "inline" was eliminated, but it still works!

--- examples/ch06/fig06_16/fig06_16.cpp
+++ examples/ch06/fig06_16/fig06_16.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // Definition of inline function cube. Definition of function appears 
 // before function is called, so a function prototype is not required. 
 // First line of function definition acts as the prototype.
-inline double cube(const double side) {
+/*inline */ double cube(const double side) {  //Keyword "inline" was eliminated, but it still works! 
    return side * side * side; // calculate cube
 }
 
